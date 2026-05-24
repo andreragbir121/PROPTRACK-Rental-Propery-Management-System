9a24b397d81177085db9fa96457bea1dceeb38af
Andre Ragbir <andreragbir121@gmail.com>
2026-05-24 14:54:51 -0400

    feat: add server routes for property ID handling and create Vercel configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'properties/:id',
+    renderMode: RenderMode.Client
+  },
+  
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
