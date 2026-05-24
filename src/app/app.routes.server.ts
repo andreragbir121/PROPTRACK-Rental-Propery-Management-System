b0dec8de352a9b6879aa659137ca154316da6eb2
Andre Ragbir <andreragbir121@gmail.com>
2026-05-24 15:08:34 -0400

    fix: reorder server routes for correct path handling

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,11 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'pin-entry',
+    renderMode: RenderMode.Client
+  },
   {
     path: 'properties/:id',
     renderMode: RenderMode.Client
-  },
-  
+  },  
   {
     path: '**',
     renderMode: RenderMode.Prerender
